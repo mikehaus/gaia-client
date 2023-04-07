9bed6240d459eed273e30426b47c1098c5e56ebf
Mike Hollingshaus <mike.haus.dev@gmail.com>
2023-04-07 08:21:26 -0600

    get q-and-a to populate through on frontend in components

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -1,4 +1,4 @@
-import { createSignal } from "solid-js";
+import { createSignal, For } from "solid-js";
 import { ChangeEvent } from "@suid/types";
 import { styled } from "@suid/material";
 import { Box, Button, TextField } from "@suid/material";
@@ -21,7 +21,20 @@ const ConversationBox = styled(Box)({
   height: "calc(100% - 100px)",
   borderRadius: 8,
   backgroundColor: COLORS.BG_CONVERSATION_BOX,
-  margin: '16px 32px'
+  margin: '16px 32px',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+})
+
+const ConversationItem = styled(Box)({
+  margin: '20px',
+  width: '95%',
+  padding: '10px',
+  fontSize: '14px',
+  backgroundColor: COLORS.BG_CONVERSATION_ITEM,
+  color: COLORS.TEXT_COLOR_CONVERSATION_ITEM,
+  borderRadius: '8px',
 })
 
 const SearchContainer = styled("div")({
@@ -40,8 +53,18 @@ const SubmitButton = styled(Button)({
   height: '48px',
 })
 
+interface ConversationItemAnswer {
+  question: string,
+  answer: string,
+}
+
+interface ConversationAnswers {
+  answers: ConversationItemAnswer[]
+}
+
 export default function MainContainer() {
   const [question, setQuestion] = createSignal("");
+  const [answers, setAnswers] = createSignal([]);
 
   const URL = `/openai/completions`;
 
@@ -61,19 +84,42 @@ export default function MainContainer() {
     setQuestion(newValue);
   }
 
-  const makeTestCall = (e: Event) => {
+  const addAnswer = (newAnswer: string): void => {
+    const conversationAnswer: ConversationItemAnswer = { question: question(), answer: newAnswer };
+    setAnswers([...answers(), conversationAnswer]);
+  }
+
+  const makeTestCall = async (e: Event) => {
     e.preventDefault();
 
-    instance.post(URL, question())
-      .then((res) => console.log(res))
-      .catch(err => console.error(err));
+    // TODO: Create response type, imply error type
+    // TODO: Add error toast if can't get response
+    // TODO: add catch based on res status
 
-    setQuestion('');
+    const res = await instance.post(URL, question());
+
+    const data = res.data;
+
+    if (data) {
+      addAnswer(data);
+      setQuestion('');
+      console.log(answers());
+    } else {
+      console.log(res);
+    }
   }
 
   return (
     <SpaContainer>
       <ConversationBox>
+        <For each={answers()}>
+          {(answer, i) =>
+            <ConversationItem >
+              <p>Question: {answer.question}</p>
+              <p>Answer: {answer.answer}</p>
+            </ConversationItem>
+          }
+        </For>
       </ConversationBox>
       <SearchContainer>
         <SearchInput
